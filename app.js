164a5c170e6f87a36acc0ba3fc9d94f4d32d16a0
STUTI MISHRA <153292796+stutimi@users.noreply.github.com>
2024-10-03 17:43:40 +0000

    first

--- app.js
+++ app.js
@@ -3,24 +3,41 @@
 
     var $navbar = $("#navbar"),
         y_pos = $navbar.offset().top,
-        height = $navbar.height();
+        height = $navbar.height(),
+        isFixed = false;
 
-    $(document).scroll(function() {
+    // Throttle function to limit scroll event firing rate
+    function throttle(func, limit) {
+        var lastFunc;
+        var lastRan;
+        return function() {
+            var context = this;
+            var args = arguments;
+            if (!lastRan) {
+                func.apply(context, args);
+                lastRan = Date.now();
+            } else {
+                clearTimeout(lastFunc);
+                lastFunc = setTimeout(function() {
+                    if (Date.now() - lastRan >= limit) {
+                        func.apply(context, args);
+                        lastRan = Date.now();
+                    }
+                }, limit - (Date.now() - lastRan));
+            }
+        };
+    }
+
+    $(document).scroll(throttle(function() {
         var scrollTop = $(this).scrollTop();
 
-    var $navbar = $("#navbar"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
-
-        if (scrollTop > y_pos + height) {
-            $navbar.addClass("navbar-fixed").animate({
-                top: 0
-            });
-        } else if (scrollTop <= y_pos) {
-            $navbar.removeClass("navbar-fixed").clearQueue().animate({
-                top: "-48px"
-            }, 0);
+        if (scrollTop > y_pos + height && !isFixed) {
+            $navbar.addClass("navbar-fixed").css("top", "0");
+            isFixed = true;
+        } else if (scrollTop <= y_pos && isFixed) {
+            $navbar.removeClass("navbar-fixed").css("top", "-48px");
+            isFixed = false;
         }
-    });
+    }, 50)); // Throttle scroll events to run once every 50 milliseconds
 
-})(jQuery, undefined);
+})(jQuery);
